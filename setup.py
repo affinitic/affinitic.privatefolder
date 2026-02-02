07065373f79166138ff130f1128928e5a180fa61
Martin Peeters <martin.peeters@affinitic.be>
2026-02-02 19:50:32 +0100

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="affinitic.privatefolder",
-    version="1.0.1",
+    version="1.0.2.dev0",
     description="Folderish content type with only one private state",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
